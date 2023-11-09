96529c5d3c394661af9db8848f2bcad3cd14c954
Anton Eryomenko <49758951+eryomenko@users.noreply.github.com>
2023-11-10 00:33:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var plansElement = document.getElementById("box-plans");
+// var homeElement = document.getElementById("secfirst");
+// // var jurisdictionElement = document.getElementById("jurisdiction");
+// // var portfolioElement = document.getElementById("portfolio");
+// // var aboutElement = document.getElementById("about");
+// // var contactsElement = document.getElementById("contacts");
+
+
+
+var btnToPlans = document.querySelector(".secfirst__button");
+// var btnToHome = document.querySelector(".menu__item-home");
+// // var btnTojurisdiction = document.querySelector(".menu__item-jurisdiction");
+// // var btnToportfolio = document.querySelector(".menu__item-portfolio");
+// // var btnToabout = document.querySelector(".menu__item-about");
+// // var btnTocontacts = document.querySelector(".menu__item-contacts");
+
+// function handleButtonClick() {
+//     plansElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // homeElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // jurisdictionElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // portfolioElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // aboutElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // contactsElement.scrollIntoView({ block: "center", behavior: "smooth" });
+// }
+
+function handleButtonClick() {
+    plansElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     homeElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // jurisdictionElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // portfolioElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // aboutElement.scrollIntoView({ block: "center", behavior: "smooth" });
+//     // contactsElement.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+
+btnToPlans.addEventListener("click", handleButtonClick);
+// btnToHome.addEventListener("click", handleButtonClick);
+// // btnTojurisdiction.addEventListener("click", handleButtonClick);
+// // btnToportfolio.addEventListener("click", handleButtonClick);
+// // btnToabout.addEventListener("click", handleButtonClick);
+// // btnTocontacts.addEventListener("click", handleButtonClick);
